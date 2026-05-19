f2a79fae5cf1c789abe225b7106200b4fb16c007
Pavel <pavelgpt8888@gmail.com>
2026-05-19 20:23:43 +0300

    Tune lint rules for MVP scaffold

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,15 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = [...nextVitals, ...nextTs];
+const eslintConfig = [
+  ...nextVitals,
+  ...nextTs,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "no-unused-vars": "off",
+    },
+  },
+];
 
 export default eslintConfig;
